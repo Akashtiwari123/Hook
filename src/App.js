e67a94cb399a351c57df19bb2271bd4180933123
Akash Tiwari <drakashti@gmail.com>
2019-11-22 12:15:16 +0530

    usestate-prop

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import "./styles.css";
 const App = () => {
   const [appState, setAppState] = useState({
     Person: [
-      { name: "Ajay", age: 21 },
+      { name: "Kunal", age: 21 },
       { name: "Harshada", age: 20 },
       { name: "Disha", age: 22 },
-      { name: "Kunal", age: 21 },
+      { name: "Ajay", age: 21 },
       { name: "Iyer", age: 24 }
-    ]
+    ],
+    changePerson: true,
+    persons: null
   });
 
   const handleClick = val => {
@@ -22,7 +24,8 @@ const App = () => {
         { name: "Disha", age: 32 },
         { name: "Kunal", age: 31 },
         { name: "Iyer", age: 64 }
-      ]
+      ],
+      changePerson: true
     });
   };
 
@@ -34,29 +37,50 @@ const App = () => {
         { name: event.target.value, age: 32 },
         { name: "Kunal", age: 31 },
         { name: "Iyer", age: 64 }
-      ]
+      ],
+      changePerson: true
     });
   };
+
+  function togglePerson() {
+    console.log(appState.changePerson);
+    appState.changePerson = !appState.changePerson;
+
+    console.log(appState.changePerson);
+  }
+
   return (
     <div>
-      <h1>Click to change age </h1>
-      <button onClick={() => handleClick()}>Click</button>
+      <button onClick={togglePerson} className="bt">
+        Toggle
+      </button>
 
-      <Person name={appState.Person[0].name} age={appState.Person[0].age} />
+      {setAppState.changePerson ? (
+        <div>
+          <center>
+            <button onClick={() => handleClick()} className="bt">
+              Click
+            </button>
+          </center>
 
-      <Person
-        click={() => handleClick("Akash")}
-        name={appState.Person[4].name}
-        age={appState.Person[4].age}
-      />
+          <Person name={appState.Person[0].name} age={appState.Person[0].age} />
 
-      <Person
-        change={HandleNameChange}
-        name={appState.Person[2].name}
-        age={appState.Person[2].age}
-      />
+          <Person
+            click={() => handleClick("Akash")}
+            name={appState.Person[4].name}
+            age={appState.Person[4].age}
+          />
+
+          <Person
+            change={HandleNameChange}
+            name={appState.Person[2].name}
+            age={appState.Person[2].age}
+          />
+        </div>
+      ) : null}
     </div>
   );
+  //console.log(appState.changePerson);
 };
 
 export default App;
